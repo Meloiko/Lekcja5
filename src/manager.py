2a20f31500bd60773b84021012dc90913829b170
Oliwia <oliwiawichl@gmail.com>
2026-03-30 12:31:03 +0200

    dodanie testów

--- src/manager.py
+++ src/manager.py
@@ -23,3 +23,18 @@ class Manager:
             if tenant.apartment not in self.apartments:
                 return False
         return True
+    def get_apartment_costs(self, apartment_key, year, month):
+        total_sum = 0.0
+        for bill in self.bills:
+            if bill.apartment == apartment_key:
+                if year is not None and month is not None:
+                    if bill.settlement_year == year and bill.settlement_month == month:
+                        total_sum += bill.amount_pln
+
+                    elif year is not None and month is None:
+                        if bill.settlement_year == year:
+                            total_sum += bill.amount_pln
+
+                    elif year is None and month is None:
+                        total_sum += bill.amount_pln
+        return float(total_sum)
