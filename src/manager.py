60712f8d083bda4e98e2f370135bfb08e033e256
Meloiko <oliwiawichl@gmail.com>
2026-03-30 23:10:49 +0200

    Add files via upload

--- src/manager.py
+++ src/manager.py
@@ -1,4 +1,4 @@
-from src.models import Apartment, Bill, Parameters, Tenant, Transfer
+from src.models import Apartment, Bill, Parameters, Tenant, Transfer, ApartmentSettlement
 
 
 class Manager:
@@ -36,3 +36,24 @@ class Manager:
 
             total_sum += bill.amount_pln
         return float(total_sum)
+
+    def create_apartment_settlement(self, apartment_key, year, month):
+        total_costs = self.get_apartment_costs(apartment_key, year, month)
+        total_transfers = self.get_apartment_transfers(apartment_key, year, month)
+
+        return ApartmentSettlement(apartment = apartment_key, year = year, month = month, total_rent_pln=total_transfers, total_bills_pln=total_costs, 
+                                   total_due_pln=total_transfers - total_costs)
+    
+    def get_apartment_transfers(self, apartment_key, year=None, month=None):
+        transfer_sum = 0.0
+        apartment_tenants = [t_id for t_id, t in self.tenants.items() if t.apartment == apartment_key]
+        for transfer in self.transfers:
+            if transfer.tenant not in apartment_tenants:
+                continue
+            if year and transfer.settlement_year != year:
+                continue
+            if month and transfer.settlement_month != month:
+                continue
+
+            transfer_sum += transfer.amount_pln
+        return float(transfer_sum)
